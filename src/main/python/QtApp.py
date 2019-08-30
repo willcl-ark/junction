091f14c3ebb98e5ef98ce900bd1d4ca3e325c32f
willcl-ark <will8clark@gmail.com>
2019-08-30 14:13:02 +0100

    RPC settings save to conf
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- src/main/python/QtApp.py
+++ src/main/python/QtApp.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import json
+from pathlib import Path
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtCore import QDateTime, Qt, QTimer
@@ -37,54 +39,62 @@ from hwilib import commands
 wallets = commands.enumerate()
 wallet_list = [wallet["type"] for wallet in wallets]
 
+here = Path(__file__).parent
+main_folder = Path(here).parent
+
 
 class Junction(QDialog):
     def __init__(self, parent=None):
         super(Junction, self).__init__(parent)
 
         self.originalPalette = QApplication.palette()
-        self.createWalletForm()
-        self.createRPCSettingsForm()
-        self.createMainTabWidget()
+        self.create_wallet_form()
+        self.create_rpc_settings_form()
+        self.create_main_tab_widget()
 
-        mainLayout = QGridLayout()
-        mainLayout.addWidget(self.mainTabWidget, 1, 0)
-        mainLayout.setRowStretch(1, 2)
-        mainLayout.setRowStretch(2, 2)
-        mainLayout.setColumnStretch(0, 1)
-        mainLayout.setColumnStretch(1, 1)
-        self.setLayout(mainLayout)
+        main_layout = QGridLayout()
+        main_layout.addWidget(self.main_tab_widget, 1, 0)
+        main_layout.setRowStretch(1, 2)
+        main_layout.setRowStretch(2, 2)
+        main_layout.setColumnStretch(0, 1)
+        main_layout.setColumnStretch(1, 1)
+        self.setLayout(main_layout)
 
         self.setWindowTitle("Hardware Wallet Junction")
         QApplication.setStyle(QStyleFactory.create("Fusion"))
         QApplication.setPalette(QApplication.style().standardPalette())
 
-    def createMainTabWidget(self):
-        self.mainTabWidget = QTabWidget()
-        self.mainTabWidget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
+    def create_main_tab_widget(self):
+        self.main_tab_widget = QTabWidget()
+        self.main_tab_widget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
 
-        tab1 = self.RPCSettingsForm
-        tab2 = self.WalletForm
-        self.mainTabWidget.addTab(tab1, "RPC Settings")
-        self.mainTabWidget.addTab(tab2, "Create Wallet")
+        tab1 = self.rpc_settings_form
+        tab2 = self.wallet_form
+        self.main_tab_widget.addTab(tab1, "RPC Settings")
+        self.main_tab_widget.addTab(tab2, "Create Wallet")
 
-    def createRPCSettingsForm(self):
-        self.RPCSettingsForm = QGroupBox("Configure testnet RPC parameters")
-        pw = QLineEdit()
-        pw.setEchoMode(QLineEdit.Password)
-        host = QLineEdit()
-        host.setText("127.0.0.1")
-        port = QLineEdit()
-        port.setText("18332")
+    def create_rpc_settings_form(self):
+        self.rpc_settings_form = QGroupBox("Configure testnet RPC parameters")
+        self.rpc_user = QLineEdit()
+        self.rpc_pw = QLineEdit()
+        self.rpc_pw.setEchoMode(QLineEdit.Password)
+        self.rpc_host = QLineEdit()
+        self.rpc_host.setText("127.0.0.1")
+        self.rpc_port = QLineEdit()
+        self.rpc_port.setText("18332")
+        self.rpc_save_btn = QPushButton("Save")
+        self.rpc_save_btn.clicked.connect(lambda: self.update_conf())
+
         layout = QFormLayout()
-        layout.addRow(QLabel("RPC Username:"), QLineEdit())
-        layout.addRow(QLabel("RPC Password:"), pw)
-        layout.addRow(QLabel("RPC Host:"), host)
-        layout.addRow(QLabel("RPC Port:"), port)
-        self.RPCSettingsForm.setLayout(layout)
+        layout.addRow(QLabel("RPC Username:"), self.rpc_user)
+        layout.addRow(QLabel("RPC Password:"), self.rpc_pw)
+        layout.addRow(QLabel("RPC Host:"), self.rpc_host)
+        layout.addRow(QLabel("RPC Port:"), self.rpc_port)
+        layout.addWidget(self.rpc_save_btn)
+        self.rpc_settings_form.setLayout(layout)
 
-    def createWalletForm(self):
-        self.WalletForm = QGroupBox("Create a new wallet")
+    def create_wallet_form(self):
+        self.wallet_form = QGroupBox("Create a new wallet")
         sig_req = QLineEdit()
         sig_req.setText("2")
         total_sig = QLineEdit()
@@ -93,11 +103,22 @@ class Junction(QDialog):
         layout.addRow(QLabel("Wallet name:"), QLineEdit())
         layout.addRow(QLabel("Signatures required:"), sig_req)
         layout.addRow(QLabel("Total signers:"), total_sig)
-        self.WalletForm.setLayout(layout)
+        self.wallet_form.setLayout(layout)
+
+    def update_conf(self):
+        with open(main_folder/"settings/settings.json") as settings:
+            data = json.load(settings)
+        data["rpc_username"] = self.rpc_user.text()
+        data["rpc_password"] = self.rpc_pw.text()
+        data["rpc_host"] = self.rpc_host.text()
+        data["rpc_port"] = self.rpc_port.text()
+        with open(main_folder/"settings/settings.json", "w") as outfile:
+            json.dump(data, outfile, indent=4)
 
 
 if __name__ == "__main__":
     appctxt = ApplicationContext()
     hwi_interface = Junction()
+    hwi_interface.resize(500, 500)
     hwi_interface.show()
     sys.exit(appctxt.app.exec_())
