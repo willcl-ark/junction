77a9c008c7f18f788f3bbdf2b4983a149939e71f
willcl-ark <will8clark@gmail.com>
2019-09-03 23:50:13 +0100

    Settings validated against bitcoind as per junction/ui.py
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -4,9 +4,10 @@ from collections import OrderedDict
 from pathlib import Path
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow, QApplication, QDialogButtonBox
+from PyQt5.QtWidgets import QMainWindow, QDialogButtonBox, QStatusBar
 
 from junction import Ui_Junction
+from utils import test_rpc
 
 
 here = Path(__file__).parent
@@ -20,9 +21,9 @@ class MainWindow(QMainWindow, Ui_Junction):
         self.RPCSettingsDialog.button(QDialogButtonBox.Save).clicked.connect(
             lambda: self.update_settings()
         )
-        self.update_RPCSettingsDialogue()
+        self.update_rpc_display()
 
-    def update_RPCSettingsDialogue(self):
+    def update_rpc_display(self):
         self.currentRPCSettingsTextBrowser.setPlainText(self.read_rpc_settings())
 
     def read_rpc_settings(self):
@@ -40,9 +41,15 @@ class MainWindow(QMainWindow, Ui_Junction):
         data["rpc_password"] = self.rPCPasswordLineEdit.text()
         data["rpc_host"] = self.rPCHostLineEdit.text()
         data["rpc_port"] = self.rPCPortLineEdit.text()
-        with open(python_src_dir / "settings/settings.json", "w") as outfile:
-            json.dump(data, outfile, indent=4)
-        self.update_RPCSettingsDialogue()
+        if test_rpc(data):
+            with open(python_src_dir / "settings/settings.json", "w") as outfile:
+                json.dump(data, outfile, indent=4)
+            self.update_rpc_display()
+            self.statusBar.showMessage("Settings validated and updated")
+        else:
+            self.statusBar.showMessage(
+                "Settings invalid and not saved -- make sure bitcoind is running"
+            )
 
 
 if __name__ == "__main__":
