21567ca6b2a08dc364b9e127441666cd6462199e
willcl-ark <will8clark@gmail.com>
2019-09-03 23:09:00 +0100

    Display and write settings.json
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from collections import OrderedDict
 from pathlib import Path
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
@@ -19,6 +20,18 @@ class MainWindow(QMainWindow, Ui_Junction):
         self.RPCSettingsDialog.button(QDialogButtonBox.Save).clicked.connect(
             lambda: self.update_settings()
         )
+        self.update_RPCSettingsDialogue()
+
+    def update_RPCSettingsDialogue(self):
+        self.currentRPCSettingsTextBrowser.setPlainText(self.read_rpc_settings())
+
+    def read_rpc_settings(self):
+        with open(python_src_dir / "settings/settings.json") as settings:
+            set_d = json.load(settings, object_pairs_hook=OrderedDict)
+            set_str = ""
+            for k, v in set_d.items():
+                set_str += f"{k}: {v}\n"
+            return set_str
 
     def update_settings(self):
         with open(python_src_dir / "settings/settings.json") as settings:
@@ -29,6 +42,7 @@ class MainWindow(QMainWindow, Ui_Junction):
         data["rpc_port"] = self.rPCPortLineEdit.text()
         with open(python_src_dir / "settings/settings.json", "w") as outfile:
             json.dump(data, outfile, indent=4)
+        self.update_RPCSettingsDialogue()
 
 
 if __name__ == "__main__":
